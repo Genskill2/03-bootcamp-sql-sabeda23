a0f1b038cc9839cb227089d5c1b599d21f01a886
Sabeda Bhanu <sabeda21@gmail.com>
2021-06-18 18:02:39 +0530

    adds table creation query

--- create.sql
+++ create.sql
@@ -1,14 +1,22 @@
+sqlite3 sample.sqlite
+
 CREATE TABLE publisher(
              id INTEGER primary key,
              name text,
              country text);
+             
+           
 CREATE TABLE books(
              id INTEGER primary key,
              title text,
              publisher INTEGER REFERENCES Publisher(id));
+             
+             
 CREATE TABLE subjects(
              id INTEGER primary key,
              name text);
+             
+             
 CREATE TABLE books_subjects(
              book INTEGER REFERENCES Books(id),
              subject INTEGER REFERENCES Subjects(id));
