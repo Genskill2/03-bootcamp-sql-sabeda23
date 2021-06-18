c156364c15ee4d98168b3e3b07bd043ff335ca9b
Sabeda Bhanu <sabeda21@gmail.com>
2021-06-18 18:20:59 +0530

    adds creation query

--- create.sql
+++ create.sql
@@ -3,5 +3,20 @@ CREATE TABLE publisher(
              name text,
              country text);
              
-           
+                       
+CREATE TABLE books(
+             id INTEGER primary key,
+             title text,
+             publisher INTEGER REFERENCES Publisher(id));
+             
+             
+CREATE TABLE subjects(
+             id INTEGER primary key,
+             name text);
+             
+             
+CREATE TABLE books_subjects(
+             book INTEGER REFERENCES Books(id),
+             subject INTEGER REFERENCES Subjects(id));
 
+             
